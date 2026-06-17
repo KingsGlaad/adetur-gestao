5659ac22b2d579e3919e580f94d9aea80f5ad7cd
Felipe <felipematheus17@hotmail.com>
2026-06-17 20:39:19 -0300

    feat: integrate pg adapter for Prisma and update database dependencies to version 7.8.0

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx scripts/create-admin.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: env("DATABASE_URL"),
   },
 });
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,12 @@
-import { PrismaClient } from "@/generated/prisma";
+import "dotenv/config";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-// Extend the global type to include prisma
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const globalForPrisma = globalThis as typeof globalThis;
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export { prisma };
